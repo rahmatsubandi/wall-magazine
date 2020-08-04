867f875a0d0054010d4196cea0ed1c37dd19daf6
rahmatsubandi <rhmtin12@gmail.com email>
2020-08-04 17:17:53 +0700

    Edit Footer

--- src/layout/Footer.js
+++ src/layout/Footer.js
@@ -2,6 +2,7 @@ import React from "react";
 import Fade from "react-reveal/Fade";
 
 import foot from "./img/foot.jpg";
+import github from "./img/github.png";
 
 export default function Footer() {
   return (
@@ -14,16 +15,23 @@ export default function Footer() {
             </div>
           </div>
           <div className="row justify-content-between">
-            <div className="col-auto">
+            <div className="col-auto pt-3">
               <h5>
                 Handcrafted <span className="text-danger">❤</span> Rahmat
                 Subandi
               </h5>
             </div>
             <div className="col-auto">
-              <nav className="nav justify-content-end">
-                <h5>2020</h5>
-              </nav>
+              <div className="justify-content-end">
+                <a
+                  href="https://github.com/rahmatsubandi"
+                  className="text-center"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  <img src={github} alt="github" style={{ width: 70 }} />
+                </a>
+              </div>
             </div>
           </div>
         </div>
